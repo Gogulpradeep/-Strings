2c5251ea38ffa1ec2d83f94a94c5931eba4f3793
gogulpradeep <mgogulpradeep@gmail.com>
2025-07-10 00:07:39 +0530

    updates

--- op.js
+++ op.js
@@ -10,3 +10,12 @@
 
  let  leng = "12345abc";
  console.log(leng.length);
+
+ let ind = "bananasplit";
+console.log(ind.indexOf("a"));
+
+let love = "l love coding";
+console.log( love.replace("coding","playing"));
+
+
+
